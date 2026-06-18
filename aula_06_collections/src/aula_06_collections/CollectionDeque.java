b3eebc221dc8e0fc63c0a5c06d4e05a17971f216
guitxc <guisandolidna@gmail.com>
2026-06-18 12:40:19 -0300

    Aula 6 - Collections Queue & Deque

---
+++ aula_06_collections/src/aula_06_collections/CollectionDeque.java
@@ -0,0 +1,27 @@
+package aula_06_collections;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class CollectionDeque {
+
+	public static void main(String[] args) {
+		Deque<String> pilha = new ArrayDeque<String>();
+		
+		pilha.push("Azul");
+		pilha.push("Verde");
+		pilha.push("Amarelo");
+		pilha.push("Branco");
+		pilha.push("Laranja");
+		
+		pilha.pop();
+		
+		System.out.println("A cor Azul ainda esta na fila? " + pilha.contains("Azul"));
+		
+		for (var cor: pilha) {
+			System.out.println(cor);
+		}
+
+	}
+
+}
---
+++ aula_06_collections/src/aula_06_exerciciosListESet/Ex1.java
@@ -0,0 +1,10 @@
+package aula_06_exerciciosListESet;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
